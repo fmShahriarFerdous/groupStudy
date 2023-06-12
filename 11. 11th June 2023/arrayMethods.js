3606b8f0d236b92cd3423d20ea2b1e56a81cb5c2
Jithan Roy <jithanroyjony@gmail.com>
2023-06-12 10:50:43 +0100

    array sort method

--- 11. 11th June 2023/arrayMethods.js
+++ 11. 11th June 2023/arrayMethods.js
@@ -1,6 +1,6 @@
 
 const arr1 = [1, 2, 3, 4, 5];
-const arr2 = [7, 8, 9, 10, 11, 12, 13, 14, 15];
+let arr2 = [7, 8, 9, 10, 11, 12, 13, 14, 15];
 const arr3 = [16, 17, 18, 19, 20, 21, 22, 23, 24, 25];
 
 let x = 100;
@@ -18,6 +18,7 @@ const res2 = [...arr1, y, ...arr2, x, ...arr3];
 // console.log(arr2);
 let res3 = arr2;
 for(let i = 1; i < 10; i++) {
-   res3 = [...res3,i];
+   arr2 = [...arr2,i];
 }
-console.log(res3);
+
+console.log(arr2);
