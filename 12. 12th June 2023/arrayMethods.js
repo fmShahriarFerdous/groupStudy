ba44d99621869f8d3e485e66c0969fb40103363c
Jithan Roy <jithanroyjony@gmail.com>
2023-06-12 10:58:34 +0100

    array sort method

---
+++ 12. 12th June 2023/arrayMethods.js
@@ -0,0 +1,17 @@
+// array flat 
+const myArr = [[1,2],[3,4],[5,6]];
+// console.log(myArr.flat());
+
+// Array Sort
+const foods = ['chickenFry', 'pizza', 'kacci','burger'];
+const numbers = [10, 40, 22, 12, 100, 16, 20, 3];
+console.log(foods.sort());
+console.log(numbers.sort());
+
+let numSort = numbers.sort((a, b) => a - b);
+console.log("numSort", numSort);
+
+// Array Reverse 
+// console.log(foods.reverse());
+// console.log(numbers.reverse());
+
