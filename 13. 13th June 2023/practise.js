7a16ed09831e3f40e158dee2bf3bfc388c5e87e5
Jithan <jithan.roy@gmail.com>
2023-06-15 01:54:58 +0600

    practise

---
+++ 13. 13th June 2023/practise.js
@@ -0,0 +1,16 @@
+const arr1 = [1, 30, 39, 29, 10, 13];
+
+// console.log(arr1.fill(0, 2, 4));
+// console.log(arr1.fill(0, 2, 4));
+
+const words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'];
+
+console.log(words.filter((e) => e.length > 5));
+
+// console.log(words.find((e, index, obj) => e === 'limit' ));
+
+
+
+const wordsFinder = words.find((e, index, obj) => console.log(obj[0]));
+
+console.log(wordsFinder);
