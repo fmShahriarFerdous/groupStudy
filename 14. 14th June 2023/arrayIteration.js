ede00fade0c90cb05eb90d0135ee60ab1a8d701a
Jithan Roy <jithanroyjony@gmail.com>
2023-06-14 08:01:54 +0100

    arrow function :

---
+++ 14. 14th June 2023/arrayIteration.js
@@ -0,0 +1,35 @@
+// Arrow function 
+// const arrow = () => console.log('Arrow function');
+// arrow();
+
+// let num = 5;
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// let num2 = (a, b) => a + b + 100;
+//     // console.log(num2(2,5));
+
+// const num3 = (a, b) => {
+//     let multiply = a * b;
+//     console.log(num2(multiply, b));
+// }
+
+// num3(1,2);
+
+const array1 = [1, 30, 39, 29, 10, 13];
+const array2 = [1,1,1];
+
+// array1.every((value, index, arr) => console.log(value, index, arr));
+// console.log(array2.every((value) => value === 1));
+
+console.log(array1.fill(4, 3, 5));
+// console.log(array2.fill(6));
+console.log(array2.fill(7,1));
+
+// filter 
+const words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'];
+console.log(words.filter((value) => value.length > 5));
+console.log(words);
+
+
+
+
