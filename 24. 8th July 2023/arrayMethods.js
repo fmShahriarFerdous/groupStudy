4bc70e103221334b47baf456cc1fc14f3898e820
fmShahriarFerdous <fmshahriar127@gmail.com>
2023-07-08 15:15:57 +0600

    ArrayFrom

---
+++ 24. 8th July 2023/arrayMethods.js
@@ -0,0 +1,15 @@
+// let arr = Array.from("12345",(num) => Number(num));
+
+// console.log(arr);
+
+let arr = [1,2,3,4,5,3];
+// let arr2 = arr.filter((num) => num > 2);
+// let arr2 = arr.find((num) =>
+// { 
+//     console.log("count");
+//     return num == 3
+// }
+//     );
+
+let arr2 = arr.fill(0,3);
+console.log("arr:",arr,"\narr2:",arr2);
