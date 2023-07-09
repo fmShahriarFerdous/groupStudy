cad5b91bf988fff10237e66a1717be374b01a8d9
fmShahriarFerdous <fmshahriar127@gmail.com>
2023-07-09 20:03:44 +0600

    Array.from,forEach,map

---
+++ 25. 9th July 2023/forEach_map.js
@@ -0,0 +1,24 @@
+let arr = "Jithan";
+let arr4 = [1,2,3,4];
+let arr5 = [{One:1},{Two:2}];
+let arr6 = {One:1,One:2};
+
+
+//Array From
+// let arr3 = Array.from(arr6,(num) => 
+// { console.log("num",num);
+//     return num;
+// })
+// console.log("arr3",arr3);
+
+//Map
+// console.log("Map---->>>");
+// let arr2 = arr4.map((num) => num);
+// console.log("arr by Map",arr2);
+
+//foreach
+console.log("forEach---->>>");
+
+
+arr4.forEach((num) => console.log(num+1));
+console.log("arr4",arr4);
