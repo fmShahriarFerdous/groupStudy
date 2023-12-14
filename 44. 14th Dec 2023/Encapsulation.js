67fcc34e290b71976f5258f0788cffce840f20de
Jithan Roy <jithanroyjony@gmail.com>
2023-12-14 06:24:40 +0000

    encapsulation js

---
+++ 44. 14th Dec 2023/Encapsulation.js
@@ -0,0 +1,25 @@
+/* Encapsulation */
+
+class Player {
+    #name;
+    #birthday;
+    constructor(name, birthday, monthlySalary, noOfMonths) {
+        this.#name = name;
+        this.#birthday = birthday;
+        this.monthlySalary = monthlySalary;
+        this.noOfMonths = noOfMonths;
+    }
+
+    calculate_age() {
+        const diff_ms = Date.now() - new Date(this.birthday).getTime();
+        const age_dt = new Date(diff_ms);
+        return Math.abs(age_dt.getUTCFullYear() - 1970);
+    }
+    getSalary() {
+        return (this.monthlySalary * this.noOfMonths).toLocaleString();
+    }
+}
+
+const maxwell = new Player("maxwell", "1980-01-01", 200000, 12);
+console.log(maxwell.getSalary());
+console.log(maxwell.calculate_age());
